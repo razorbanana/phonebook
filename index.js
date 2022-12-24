7819768aea1ff9697c7756e40e0601ce93cda8d4
razorbanana <stanislav.stefyuk2@gmail.com>
2022-12-24 21:56:57 +0200

    feat: 3.3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require("express")
 const express = require("express")
 const app = express()
 app.use(express.json())
@@ -33,6 +34,15 @@ app.get('/api/persons', (request, response)=>{
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if(person){
+        response.json(person)
+    }else{
+        response.status(404).end()
+    }
+})
 
 
 const PORT = 3001
